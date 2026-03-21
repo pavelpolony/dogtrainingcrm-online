a3f7ecf700244ca1a60bdfde99c088f56540895f
pavelpolony <pavel.polony@gmail.com>
2026-03-21 12:32:37 +0100

    Add translation context processor
    Added a context processor to inject a translation function.

--- app.py
+++ app.py
@@ -91,3 +91,11 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(host="0.0.0.0", port=5000)
+
+
+@app.context_processor
+def inject_translator():
+    def t(key):
+        return key
+    return dict(t=t)
+
